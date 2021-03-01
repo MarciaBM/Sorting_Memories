d5b1d05b67eff82d01f2513c570c5dda4ac8482d
MárciaBM <marciab.matias@hotmail.com>
2021-03-01 19:44:30 +0000

    1.2 version

--- src/Main.java
+++ src/Main.java
@@ -1,123 +1,148 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+    import java.io.BufferedReader;
+    import java.io.File;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.*;
 
-public class Main {
+    public class Main {
 
-    private static boolean isDuplicatedProcess(String pathFile1, String pathFile2) throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("diff", pathFile1, pathFile2);
-        Process process = processBuilder.start();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line= reader.readLine();
-        return line == null;
-    }
-
-    private static int deleteEmptyFolders(File folder, int counter) {
-        if (folder.isDirectory()) {
-            if(folder.listFiles().length > 0) {
-                File[] folders = folder.listFiles();
-                for (File insideFolder : folders) {
-                    counter = deleteEmptyFolders(insideFolder, counter);
-                }
-            }
-            if (folder.listFiles().length == 0) {
-                folder.delete();
-                counter++;
-            }
-        }
-        return counter;
-    }
-
-    private static File getFile(File file) {
-        if(file.isFile()) {
-            return file;
-        }else{
-            File[] files=file.listFiles();
-            for(File f:files)
-                return getFile(f);
-        }
-        return null;
-    }
-
-    private static void deleteDuplicatedFiles(Scanner in,File folder) throws IOException{
-        Map<File,Boolean> files = new HashMap<>();
-        for(File f:folder.listFiles()) {
-            Iterator<File> it = getFile(f, new ArrayList<>());
-            while(it.hasNext())
-                files.put(it.next(), false);
+        private static boolean isDuplicatedProcess(String pathFile1, String pathFile2) throws IOException {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.command("diff", pathFile1, pathFile2);
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line= reader.readLine();
+            return line == null;
         }
 
-        File current;
-        List<File> files = new LinkedList<>();
-        filesIt.forEachRemaining(files::add);
-
-        for(int i = 0;i<files.size()-1;i++) {
-            File file = list.get(i);
-            if (!files.get(file)) {
-                current = file;
-                List<File> toDelete = new ArrayList<>();
-                toDelete.add(current);
-
-                for (int j = i + 1; j < files.size(); j++) {
-                    File secondFile = list.get(j);
-                    if (isDuplicatedProcess(current.getAbsolutePath(), secondFile.getAbsolutePath()))
-                        toDelete.add(secondFile);
-                }
-
-
-                if (toDelete.size() > 1) {
-                    System.out.println("Duplicated files have been found, please choose the ones you want to keep (separate them with space):");
-                    for (int m = 0; m < toDelete.size(); m++) {
-                        int aux = m + 1;
-                        System.out.println(aux + ": " + toDelete.get(m).getAbsolutePath());
+        private static int deleteEmptyFolders(File folder, int counter) {
+            if (folder.isDirectory()) {
+                if(folder.listFiles().length > 0) {
+                    File[] folders = folder.listFiles();
+                    for (File insideFolder : folders) {
+                        counter = deleteEmptyFolders(insideFolder, counter);
                     }
-                    System.out.println("E - Keep them all");
-                    String answer = in.nextLine();
-                    if (!answer.equals("E")) {
-                        String[] numbers = answer.split(" ");
-                        for (String s : numbers) {
-                            int index = Integer.parseInt(s) - 1;
-                            files.put(toDelete.get(index),true);
+                }
+                if (folder.listFiles().length == 0) {
+                    folder.delete();
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        private static Iterator<File> getFile(File file, List<File> recursiveFileList) {
+            if(file.isFile()) {
+                recursiveFileList.add(file);
+            }else{
+                for(File f:file.listFiles())
+                    getFile(f,recursiveFileList);
+            }
+            return recursiveFileList.iterator();
+        }
+
+        private static void deleteDuplicatedFiles(Scanner in,File folder) throws IOException{
+            Map<File, FileProperties> files = new HashMap<>();
+            for(File f:folder.listFiles()) {
+                Iterator<File> it = getFile(f, new ArrayList<>());
+                while(it.hasNext())
+                    files.put(it.next(), new FileProperties(false,false));
+            }
+
+            File current;
+            List<File> list = new ArrayList<>(files.keySet());
+
+            for(int i = 0;i<files.size()-1;i++) {
+                File file = list.get(i);
+                if (!files.get(file).getSeen() && !files.get(file).getToDelete()) {
+                    current = file;
+                    List<File> toDelete = new ArrayList<>();
+                    toDelete.add(current);
+
+                    for (int j = i + 1; j < files.size(); j++) {
+                        File secondFile = list.get(j);
+                        if(!files.get(secondFile).getSeen() && !files.get(secondFile).getToDelete()) {
+                            if (isDuplicatedProcess(current.getAbsolutePath(), secondFile.getAbsolutePath())) {
+                                toDelete.add(secondFile);
+                                files.get(secondFile).setSeen(true);
+                            }
+                        }
+                    }
+
+
+                    if (toDelete.size() > 1) {
+                        System.out.println("Duplicated files have been found, please choose the ones you want to delete (separate them with space):");
+                        for (int m = 0; m < toDelete.size(); m++) {
+                            int aux = m + 1;
+                            System.out.println(aux + ": " + toDelete.get(m).getAbsolutePath());
+                        }
+                        System.out.println("E: Keep them all");
+                        String answer = in.nextLine();
+                        if (!answer.equals("E")) {
+                            String[] numbers = answer.split(" ");
+                            for (String s : numbers) {
+                                int index = Integer.parseInt(s) - 1;
+                                files.get(toDelete.get(index)).setToDelete(true);
+                            }
                         }
                     }
                 }
             }
-        }
 
-        Iterator<File> keys = files.keySet().iterator();
-        while(keys.hasNext()){
-            File key = keys.next();
-            if(files.get(key))
-                key.delete();
-        }
-    }
-
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        String directory = args[0];
-        File folder = new File(directory);
-
-        if(folder.isDirectory()) {
-
-            //delete empty folders
-            System.out.println(deleteEmptyFolders(folder,0) + " empty folders were deleted.");
-
-            try {
-                //real shit
-                folder = new File(directory);
-                deleteDuplicatedFiles(in, folder);
-                System.out.println(deleteEmptyFolders(folder, 0) + " empty folders were deleted.");
-
-
-            } catch (Exception e) {
-                System.out.println(e);
-
+            for (File key : files.keySet()) {
+                if (files.get(key).getToDelete())
+                    key.delete();
             }
-        }else
-            System.out.println("Please insert a folder's directory");
+        }
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            String directory = args[0];
+            File folder = new File(directory);
+
+            if(folder.isDirectory()) {
+
+                //delete empty folders
+                System.out.println(deleteEmptyFolders(folder,0) + " empty folders were deleted.");
+
+                try {
+                    //real shit
+                    folder = new File(directory);
+                    deleteDuplicatedFiles(in, folder);
+                    System.out.println(deleteEmptyFolders(folder, 0) + " empty folders were deleted.");
+
+
+                } catch (Exception e) {
+                    System.out.println(e);
+
+                }
+            }else
+                System.out.println("Please insert a folder's directory");
+        }
     }
-}
+
+    class FileProperties{
+        private boolean toDelete;
+        private boolean seen;
+        public FileProperties(boolean toDelete, boolean seen){
+            this.seen = seen;
+            this.toDelete = toDelete;
+        }
+
+        public boolean getToDelete(){
+            return toDelete;
+        }
+
+        public boolean getSeen(){
+            return seen;
+        }
+
+        public void setToDelete(boolean toDelete){
+            this.toDelete = toDelete;
+        }
+
+        public void setSeen(boolean seen){
+            this.seen = seen;
+        }
+    }
